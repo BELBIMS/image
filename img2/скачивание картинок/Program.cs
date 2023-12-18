16c173025a77cc0928b3d1efcb15b2ff113af28c
Душин Максим <60781555+BELBIMS@users.noreply.github.com>
2023-12-18 11:49:55 +0300

    Update Program.cs

--- img2/скачивание картинок/Program.cs
+++ img2/скачивание картинок/Program.cs
@@ -4,12 +4,12 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        HttpClient client = new();
+        HttpClient client = new();       // Создаем экземпляр класса HttpClient для выполнения HTTP-запросов
         Console.WriteLine("Введите ссылку для скачивания картинки: ");
-        string nameWebsite = Console.ReadLine();
-        HttpResponseMessage response = await client.GetAsync(nameWebsite);
-        byte[] data = await response.Content.ReadAsByteArrayAsync();
-        await File.WriteAllBytesAsync("C:\\Users\\Пользователь\\Desktop\\b.jpg", data);
-        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\Пользователь\\Desktop\\b.jpg", UseShellExecute = true });
+        string nameWebsite = Console.ReadLine();                // Запрашиваем у пользователя ссылку для скачивания картинки
+        HttpResponseMessage response = await client.GetAsync(nameWebsite);         // Выполняем асинхронный HTTP-запрос и получаем ответ
+        byte[] data = await response.Content.ReadAsByteArrayAsync();       // Считываем данные из ответа в байтовый массив
+        await File.WriteAllBytesAsync("C:\\Users\\Пользователь\\Desktop\\b.jpg", data);            // Записываем данные в файл "C:\\Users\\Пользователь\\Desktop\\b.jpg"
+        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\Пользователь\\Desktop\\b.jpg", UseShellExecute = true });        // Открываем сохраненный файл с помощью процесса
     }
 }
